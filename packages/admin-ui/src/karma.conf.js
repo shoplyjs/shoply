ea0d681b323a6b4a2f83564357871de292a92c6e
ArseniyDev <jkl@usa.com>
2024-11-28 10:01:58 +0100

    chore: Update TypeScript configuration across projects and resolve problems

--- packages/admin-ui/src/karma.conf.js
+++ packages/admin-ui/src/karma.conf.js
@@ -3,17 +3,17 @@
 
 process.env.CHROME_BIN = require('puppeteer').executablePath();
 
-module.exports = function(config) {
+module.exports = function (config) {
     config.set({
         basePath: '',
         frameworks: ['jasmine', '@angular-devkit/build-angular'],
         plugins: [
-            require('karma-jasmine'),
-            require('karma-chrome-launcher'),
-            require('karma-jasmine-html-reporter'),
-            require('karma-mocha-reporter'),
-            require('karma-coverage-istanbul-reporter'),
-            require('@angular-devkit/build-angular/plugins/karma'),
+            'karma-jasmine',
+            'karma-chrome-launcher',
+            'karma-jasmine-html-reporter',
+            'karma-mocha-reporter',
+            'karma-coverage-istanbul-reporter',
+            '@angular-devkit/build-angular/plugins/karma.js',
         ],
         client: {
             clearContext: false, // leave Jasmine Spec Runner output visible in browser
